362df78e0c059818f0a24ac98107879520124709
never1068 <jlii0133@student.monash.edu>
2019-09-15 19:10:38 +1000

    task2
    task2

---
+++ ass2/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ass2.Startup))]
+namespace ass2
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
